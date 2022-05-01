69687c986906d9a58db762fbc9c338d5cfa37814
PrathimaGopal <gopalprathi@gmail.com>
2022-04-30 19:02:29 -0700

    fix button styles

--- cmpe280-eventplanner/frontend/src/components/event/Events.jsx
+++ cmpe280-eventplanner/frontend/src/components/event/Events.jsx
@@ -4,6 +4,9 @@ import Catering from "../catering/Catering";
 import EventInput from "./EventInput";
 import Decoration from "../decoration/Decoration";
 import Addons from "../addons/Addons";
+import "antd/dist/antd.min.css";
+import { Button } from "antd";
+import { FastForwardFilled, FastBackwardFilled } from "@ant-design/icons";
 
 export default function Events() {
   const [page, setPage] = useState(0);
@@ -29,13 +32,7 @@ export default function Events() {
     "Addons",
     "Cart",
   ];
-  const NavTitles = [
-    "Event Details",
-    "Catering",
-    "Decoration",
-    "Addons",
-    "Submit",
-  ];
+  const NavTitles = ["Events", "Catering", "Decoration", "Addons", "Submit"];
 
   const PageDisplay = () => {
     if (page === 0) {
@@ -68,17 +65,25 @@ export default function Events() {
       <div className="form-Container">
         <div className="body">{PageDisplay()}</div>
         <div className="footer">
-          <button
-            className="eventSubmit"
-            disabled={page === 0}
+          <Button
+            type="primary"
+            shape="round"
+            style={{ width: "140px" }}
+            icon={<FastBackwardFilled />}
+            size="large"
             onClick={() => {
               setPage((currPage) => currPage - 1);
             }}
           >
-            &lt;&lt; {NavTitles[page - 1]}
-          </button>
-          <button
-            className="eventSubmit"
+            {NavTitles[page - 1]}
+          </Button>
+          <span style={{ width: "5px" }} />
+          <Button
+            type="primary"
+            shape="round"
+            style={{ width: "140px" }}
+            icon={<FastForwardFilled />}
+            size="large"
             onClick={() => {
               if (page === FormTitles.length - 1) {
                 alert("FORM SUBMITTED");
@@ -88,8 +93,8 @@ export default function Events() {
               }
             }}
           >
-            {NavTitles[page + 1]} &gt;&gt;
-          </button>
+            {NavTitles[page + 1]}
+          </Button>
         </div>
       </div>
     </div>
